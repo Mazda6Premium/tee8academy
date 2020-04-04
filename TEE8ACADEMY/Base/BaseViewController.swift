cc4ef3e7125ff1ca182c0038cddb62c4a69a16ae
TrungBeo <nttrung254@gmail.com>
2020-04-04 09:46:39 +0700

    starting project 4/4/2020

---
+++ TEE8ACADEMY/Base/BaseViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BaseViewController.swift
+//  TEE8ACADEMY
+//
+//  Created by Trung iOS on 4/4/20.
+//  Copyright © 2020 Fighting. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class BaseViewController: UIViewController {
+    
+}
